b26298ad945d94e69c3f6e48d202f1cfda49cc78
benh4000 <strawberry4001@gmail.com>
2025-01-12 01:34:39 -0600

    test

--- src/main/java/io/github/shadowdevsthings/pseudoscience/Pseudoscience.java
+++ src/main/java/io/github/shadowdevsthings/pseudoscience/Pseudoscience.java
@@ -11,6 +11,7 @@ public class Pseudoscience implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
 
+	//.
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
